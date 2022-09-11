568a6a9b5da5b92b013b345e8cbea8508998997c
Giles Dring <giles@dringtech.com>
2022-09-11 22:26:26 +0000

    Remove dependency on auto_mnodel

--- datagen/generator.py
+++ datagen/generator.py
@@ -2,14 +2,12 @@
 from factory import make_factory
 from pandas import DataFrame
 
-from .auto_model import AutoModel
-
 
 class Generator:
     '''Generator class'''
 
     def __init__(self, **kwargs):
-        self.factory = make_factory(AutoModel, **kwargs)
+        self.factory = make_factory(dict, **kwargs)
         self.data = None
 
     def build_batch(self, count):
@@ -18,9 +16,7 @@ class Generator:
 
     def save_csv(self, filename):
         '''Write generated data to file'''
-        data_frame = DataFrame.from_records([
-            d.props for d in self.data
-        ])
+        data_frame = DataFrame.from_records(self.data)
         data_frame.to_csv(filename)
 
     def __repr__(self):
