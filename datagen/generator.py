d17d770cfd6d136501631e368b22601179a5bde1
Giles Dring <giles@dringtech.com>
2022-09-11 22:08:34 +0000

    Mild refactoring

--- datagen/generator.py
+++ datagen/generator.py
@@ -12,15 +12,16 @@ class Generator:
         self.factory = make_factory(AutoModel, **kwargs)
         self.data = None
 
-    def create_batch(self, count):
+    def build_batch(self, count):
         '''Generate a new batch'''
-        self.data = DataFrame.from_records([
-            d.props for d in self.factory.create_batch(count)
-        ])
+        self.data = self.factory.build_batch(count)
 
     def save_csv(self, filename):
         '''Write generated data to file'''
-        self.data.to_csv(filename)
+        data_frame = DataFrame.from_records([
+            d.props for d in self.data
+        ])
+        data_frame.to_csv(filename)
 
     def __repr__(self):
         return repr(self.data)
--- register_of_members_interests.py
+++ register_of_members_interests.py
@@ -5,14 +5,25 @@ import factory
 
 import datagen
 
+LOCALE = "en_GB"
+
 members = datagen.Generator(
     id=factory.Sequence(lambda n: n),
     firstname=factory.Faker('first_name'),
     lastname=factory.Faker('last_name')
 )
 
+constituency = datagen.Generator(
+    id=factory.Sequence(lambda n: n),
+    name=factory.Faker('administrative_unit', locale=LOCALE),
+)
+
+
 if __name__ == "__main__":
-    os.chdir('working')
-    members.create_batch(10)
-    print(members)
-    members.save_csv('members.csv')
+    with factory.debug():
+        os.chdir('working')
+
+        constituency.build_batch(10)
+        members.build_batch(10)
+        print(members)
+        members.save_csv('members.csv')
